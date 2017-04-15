283ffcc124415561b7be200b3f33c29da8f64856
Glowies <comokt.18@robcol.k12.tr>
2017-04-15 17:44:34 +0300

    Token Verification

--- index.js
+++ index.js
@@ -194,9 +194,6 @@ io.on('connection', function(socket){
             host: 'www.googleapis.com',
             path: '/oauth2/v1/tokeninfo?access_token=' + data
         }, function(res) {
-            console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-
             // Buffer the body entirely for processing as a whole.
             var bodyChunks = [];
             res.on('data', function(chunk) {
@@ -205,30 +202,7 @@ io.on('connection', function(socket){
             }).on('end', function() {
                 var body = Buffer.concat(bodyChunks);
                 userid = body;
-                console.log('BODY: ' + body);
-                // ...and/or process the entire body here.
-            })
-        });
-
-        req.on('error', function(e) {
-            console.log('ERROR: ' + e.message);
-        });
-
-        var req = https.get({
-            host: 'www.googleapis.com',
-            path: '/plus/v1/people/' + userid
-        }, function(res) {
-            console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-            // Buffer the body entirely for processing as a whole.
-            var bodyChunks = [];
-            res.on('data', function(chunk) {
-                // You can process streamed parts here...
-                bodyChunks.push(chunk);
-            }).on('end', function() {
-                var body = Buffer.concat(bodyChunks);
-                console.log('BODY: ' + body);
+                console.log('Token Verification: ' + body);
                 // ...and/or process the entire body here.
             })
         });
